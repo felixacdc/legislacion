d0441d98ede7ad54cc325f2db15e38a753c8d891
felixacdc <pfmata01@gmail.com>
2016-04-19 01:04:36 -0600

    [modify] fix design footer

--- app/resources/views/partials/footer.blade.php
+++ app/resources/views/partials/footer.blade.php
@@ -3,28 +3,18 @@
         <div class="row text-center">
             <div class="footer-content">
                 <div class="wow animated fadeInDown">
-                    <p>newsletter signup</p>
-                    <p>Get Cool Stuff! We hate spam!</p>
+                    <p>Pablo Félix Méndez Argueta</p>
+                    <p>Pueden contactarme mediante:</p>
                 </div>
-                <form action="#" method="post" class="subscribe-form wow animated fadeInUp">
-                    <div class="input-field">
-                        <input type="email" class="subscribe form-control" placeholder="Enter Your Email...">
-                        <button type="submit" class="submit-icon">
-                            <i class="fa fa-paper-plane fa-lg"></i>
-                        </button>
-                    </div>
-                </form>
                 <div class="footer-social">
                     <ul>
                         <li class="wow animated zoomIn"><a href="#"><i class="fa fa-thumbs-up fa-3x"></i></a></li>
                         <li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="#"><i class="fa fa-skype fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="#"><i class="fa fa-dribbble fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="1.2s"><a href="#"><i class="fa fa-youtube fa-3x"></i></a></li>
+                        <li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="#"><i class="fa fa-github fa-3x"></i></a></li>
+                        <li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="#"><i class="fa fa-google-plus fa-3x"></i></a></li>
                     </ul>
                 </div>
 
-                <p>Design And developed By <a href="http://themefisher.com/">Themefisher</a>. All rights Reserved</p>
             </div>
         </div>
     </div>
