57232353625b9832b48cde255deaa1d7387842f8
felixacdc <pfmata01@gmail.com>
2016-04-30 23:15:11 -0600

    [add] comments in articles 15, 18 and 19 (fix spelling)

--- app/database/seeds/CommentsTableSeeder.php
+++ app/database/seeds/CommentsTableSeeder.php
@@ -18,13 +18,13 @@ class CommentsTableSeeder extends Seeder
             <span class="opNum">3.</span> Tercer principio: principio irrenunciable<br>
             <span class="opNum">4.</span> Cuarto principio: Principio de superación de derecho mínimo.',
             'description2' => '<span class="opNum">5.</span> Quinto principio: Principio de imperatividad<br>
-            <span class="opNum">6.</span> Sexto principio: Principio de la Autonomia de la Voluntad Civil (El principio de imperatividad limita al principio de la Autonomia de la Voluntad Civil)',
-            'description3' => '<span class="opNum">7.</span> Septimo principio: Principio realista<br>
-            <span class="opNum">8.</span> Octabo principio: Principio objetivo<br>
-            <span class="opNum">9.</span> Noveno principio: Principio de la naturaleza juridica',
-            'description4' => '<span class="opNum">10.</span> Decimo principio: Derecho ondeamente democratico<br>
-            <span class="opNum">11.</span> Onceabo Principio: Principio de conciliación (Agregado)<br>
-            <span class="opNum">12.</span> Doceabo Principio: IN DUBIO PRO OPERARIO Articulo 17 Codigo de trabajo <a href="#codeWork" data-search="Artículo 17" class="linkGo">Ir a Artículo 17.</a>',
+            <span class="opNum">6.</span> Sexto principio: Principio de la Autonomía de la Voluntad Civil (El principio de imperatividad limita al principio de la Autonomía de la Voluntad Civil)',
+            'description3' => '<span class="opNum">7.</span> Séptimo principio: Principio realista<br>
+            <span class="opNum">8.</span> Octavo principio: Principio objetivo<br>
+            <span class="opNum">9.</span> Noveno principio: Principio de la naturaleza jurídica',
+            'description4' => '<span class="opNum">10.</span> Décimo principio: Derecho ondeamente democrático<br>
+            <span class="opNum">11.</span> Onceavo Principio: Principio de conciliación (Agregado)<br>
+            <span class="opNum">12.</span> Doceavo Principio: IN DUBIO PRO OPERARIO Articulo 17 Código de trabajo <a href="#codeWork" data-search="Artículo 17" class="linkGo">Ir a Artículo 17.</a>',
             'article_id' => '1'
         ]);
 
@@ -36,7 +36,7 @@ class CommentsTableSeeder extends Seeder
             <li><span class="opNum">1.</span> Auxiliares del patrono:</li>
                 <ul><li><span class="opNum">2.</span> Representante del patrono</li>
                 <li>Empleados de confianza <a href="#codeWork" data-search="Artículo 351" class="linkGo">Ir a Artículo 351</a></li>
-                <li>Cargos de direccion <a href="#codeWork" data-search="Artículo 315" class="linkGo">Ir a Artículo 315</a></li>
+                <li>Cargos de dirección <a href="#codeWork" data-search="Artículo 315" class="linkGo">Ir a Artículo 315</a></li>
                 <li>Intermediario <a href="#codeWork" data-search="Artículo 5" class="linkGo">Ir a Artículo 5.</a></li></ul>
             </ol>',
             'article_id' => '2'
@@ -50,7 +50,7 @@ class CommentsTableSeeder extends Seeder
             <li>Auxiliares del patrono: <a href="#codeWork" data-search="Artículo 4" class="linkGo">Ir a Artículo 4.</a></li>
                 <ul><li>Representante del patrono <a href="#codeWork" data-search="Artículo 4" class="linkGo">Ir a Artículo 4.</a></li>
                 <li>Empleados de confianza <a href="#codeWork" data-search="Artículo 351" class="linkGo">Ir a Artículo 351</a></li>
-                <li>Cargos de direccion <a href="#codeWork" data-search="Artículo 315" class="linkGo">Ir a Artículo 315</a></li>
+                <li>Cargos de dirección <a href="#codeWork" data-search="Artículo 315" class="linkGo">Ir a Artículo 315</a></li>
                 <li><span class="opNum">1.</span> Intermediario</li></ul>
             </ol>',
             'article_id' => '3'
@@ -64,15 +64,33 @@ class CommentsTableSeeder extends Seeder
 
         DB::table('comments')->insert([
             'description1' => '<strong>Nulidad del contrato individual del trabajo:</strong><br>
-            <p>El articulo 12 contiene el fundamento de la nulidad del contrato individual del trabajo y el pleno derecho.</p>
-            <p><strong>Ipso Jure: </strong>"de pleno derecho" o "de pleno derecho por poder potestad o ministerio de la ley."</p>',
+            El articulo 12 contiene el fundamento de la nulidad del contrato individual del trabajo y el pleno derecho.<br>
+            <strong>Ipso Jure: </strong>"de pleno derecho" o "de pleno derecho por poder potestad o ministerio de la ley."',
             'article_id' => '5'
         ]);
 
         DB::table('comments')->insert([
+            'description1' => '<strong>El articulo 15 responde la siguiente interrogante:</strong><br>
+            <span class="opNum">1.</span> ¿Como se integra el sistema de interpretación de las normas?',
+            'article_id' => '6'
+        ]);
+
+        DB::table('comments')->insert([
             'description1' => '<strong>Principios informativos del derecho de trabajo :</strong> <a href="#codeWork" data-search="Considerando" class="linkGo">Ir a Considerando.</a><br>
-            <span class="opNum">1.</span> Doceabo Principio: IN DUBIO PRO OPERARIO',
+            <span class="opNum">1.</span> Doceavo Principio: IN DUBIO PRO OPERARIO',
             'article_id' => '7'
+        ]);
+
+        DB::table('comments')->insert([
+            'description1' => '<strong>Definición del contrato de trabajo:</strong><br>
+            <span class="opNum">1.</span> Contrato de trabajo',
+            'article_id' => '8'
+        ]);
+
+        DB::table('comments')->insert([
+            'description1' => '<span class="opNum">1.</span> Lo que debe suceder para que el contrato individual de trabajo exista.<br>
+            <span class="opNum">2.</span> Definición de relación de trabajo.',
+            'article_id' => '9'
         ]);
 
         DB::table('comments')->insert([
@@ -83,7 +101,7 @@ class CommentsTableSeeder extends Seeder
             <li>Auxiliares del patrono: <a href="#codeWork" data-search="Artículo 4" class="linkGo">Ir a Artículo 4.</a></li>
                 <ul><li>Representante del patrono <a href="#codeWork" data-search="Artículo 4" class="linkGo">Ir a Artículo 4.</a></li>
                 <li>Empleados de confianza</li>
-                <li><span class="opNum">1.</span> Cargos de direccion</li>
+                <li><span class="opNum">1.</span> Cargos de dirección</li>
                 <li>Intermediario <a href="#codeWork" data-search="Artículo 5" class="linkGo">Ir a Artículo 5.</a></li></ul>
             </ol>',
             'article_id' => '21'
@@ -97,7 +115,7 @@ class CommentsTableSeeder extends Seeder
             <li>Auxiliares del patrono: <a href="#codeWork" data-search="Artículo 4" class="linkGo">Ir a Artículo 4.</a></li>
                 <ul><li>Representante del patrono <a href="#codeWork" data-search="Artículo 4" class="linkGo">Ir a Artículo 4.</a></li>
                 <li><span class="opNum">1.</span> Empleados de confianza</li>
-                <li>Cargos de direccion</li>
+                <li>Cargos de dirección</li>
                 <li>Intermediario <a href="#codeWork" data-search="Artículo 5" class="linkGo">Ir a Artículo 5.</a></li></ul>
             </ol>',
             'article_id' => '22'
