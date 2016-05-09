592a62ebd419130d18515597d96e10fccf81cbb9
felixacdc <pfmata01@gmail.com>
2016-05-08 22:45:04 -0600

    [modify] query of CodeWork and CodeCommercial

--- app/app/Http/Controllers/CodesController.php
+++ app/app/Http/Controllers/CodesController.php
@@ -21,21 +21,25 @@ class CodesController extends Controller
     {
         $articles = Codes::find(1)
                     ->articles()
-                    ->where('number', 'LIKE', '%' . $search . '%')
-                    ->orWhere('name', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description1', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description2', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description3', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description4', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description5', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description6', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description7', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description8', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description9', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description10', 'LIKE', '%' . $search . '%')
-                    ->get();
+                    ->where('code_id', "=", 1)
+                    ->where(function($query) use ($search){
+                        return $query
+                            ->where('number', 'LIKE', '%' . $search . '%')
+                            ->orWhere('name', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description1', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description2', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description3', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description4', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description5', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description6', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description7', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description8', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description9', 'LIKE', '%' . $search . '%')
+                            ->orWhere('description10', 'LIKE', '%' . $search . '%');
+                    })->get();
 
         if ($request->ajax()) {
+            
             return response()->json([
                 'data' => $articles->toArray()
             ]);
@@ -61,20 +65,7 @@ class CodesController extends Controller
                             ->orWhere('description8', 'LIKE', '%' . $search . '%')
                             ->orWhere('description9', 'LIKE', '%' . $search . '%')
                             ->orWhere('description10', 'LIKE', '%' . $search . '%');
-                    })
-                    /*->where('number', 'LIKE', '%' . $search . '%')
-                    ->orWhere('name', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description1', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description2', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description3', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description4', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description5', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description6', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description7', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description8', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description9', 'LIKE', '%' . $search . '%')
-                    ->orWhere('description10', 'LIKE', '%' . $search . '%')*/
-                    ->get();
+                    })->get();
 
         if ($request->ajax()) {
             return response()->json([
