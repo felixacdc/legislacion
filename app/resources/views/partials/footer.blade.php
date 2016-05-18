47a53da22e94e36673b6f206993ac704bab5a21f
felixacdc <pfmata01@gmail.com>
2016-05-18 09:58:10 -0600

    [fix] links of about and footer

--- app/resources/views/partials/footer.blade.php
+++ app/resources/views/partials/footer.blade.php
@@ -8,10 +8,9 @@
                 </div>
                 <div class="footer-social">
                     <ul>
-                        <li class="wow animated zoomIn"><a href="#"><i class="fa fa-thumbs-up fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="#"><i class="fa fa-github fa-3x"></i></a></li>
-                        <li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="#"><i class="fa fa-google-plus fa-3x"></i></a></li>
+                        <li class="wow animated zoomIn"><a href="https://www.facebook.com/pablo.mendez.5872" target="_blank"><i class="fa fa-thumbs-up fa-3x"></i></a></li>
+                        <li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="https://twitter.com/felix23mendez" target="_blank"><i class="fa fa-twitter fa-3x"></i></a></li>
+                        <li class="wow animated zoomIn" data-wow-delay="0.9s"><a href="https://plus.google.com/104696139021644769950" target="_blank"><i class="fa fa-google-plus fa-3x"></i></a></li>
                     </ul>
                 </div>
 
--- app/resources/views/partialsHome/social.blade.php
+++ app/resources/views/partialsHome/social.blade.php
@@ -5,14 +5,12 @@
             <div class="row">
 
                 <div class="sec-title text-center white wow animated fadeInDown">
-                    <h2>FOLLOW US</h2>
-                    <p>Beautifully simple follow buttons to help you get followers on</p>
+                    <h2>UMG</h2>
+                    <p>Universidad Mariano Galvez</p>
                 </div>
 
                 <ul class="social-button">
-                    <li class="wow animated zoomIn"><a href="#"><i class="fa fa-thumbs-up fa-2x"></i></a></li>
-                    <li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="#"><i class="fa fa-twitter fa-2x"></i></a></li>
-                    <li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="#"><i class="fa fa-dribbble fa-2x"></i></a></li>
+                    <li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="https://www.umg.edu.gt/" target="_blank"><i class="fa fa-dribbble fa-2x"></i></a></li>
                 </ul>
 
             </div>
